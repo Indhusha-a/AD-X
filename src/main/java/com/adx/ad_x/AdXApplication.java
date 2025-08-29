7f11ea34d43ec6d521a5781e935d23ce758add45
Indhusha <it24101254@my.sliit.lk>
2025-08-30 02:29:35 +0530

    Project Setup and Beginning

---
+++ src/main/java/com/adx/ad_x/AdXApplication.java
@@ -0,0 +1,13 @@
+package com.adx.ad_x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdXApplication.class, args);
+    }
+
+}
